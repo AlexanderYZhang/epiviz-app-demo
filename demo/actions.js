00bd3d92dfa8b5eede1e7255df72a5c7f3059b23
AlexanderYZhang <alexzhangn@gmail.com>
2016-12-22 15:36:51 -0500

    separated files

---
+++ demo/actions.js
@@ -0,0 +1,131 @@
+window.onload = function() {
+    $('#testing').range({
+        min: 0,
+        max: 10,
+        start:5
+    });
+    var graphs = ["Blocks Track", "Line Track", "Stacked Track", "Genes Track", "Scatter Plot", "Heatmap", "Line Plot", "Stacked Plot", "Icicle"];
+    var div = document.getElementById("menu");
+    graphs.forEach(function(graph) {
+        var item = document.createElement('div');
+        var text = document.createElement('a');
+        text.text = graph; 
+        text.id = "item";
+        item.className = "item";
+        item.appendChild(text);
+        div.appendChild(item);
+    });
+    $('.menu .item').tab();
+
+    $(document).on('click', '#modalbutton', function() {
+        $('#newmodal').show();
+        loadMeasurements();
+    });
+    $(document).on('click', '#item', function() {
+        graph = $(this).text().toLowerCase();
+        graph = graph.replace(" ", "-");
+        load();
+        $('#modal').show();
+    })
+    // $(document).on('click', '.ui.checkbox input', function(e) {
+    //     e.stopPropogation();
+    //     console.log("clicked");
+    //     var split = this.id.split('-');
+    //     if (split[0] === "source") {
+    //         var ids = $('[id$=' + split[1] + ']');
+    //         ids.checkbox('check');
+    //     }   
+
+    // });
+}
+function attachActions() {
+    $('#button').click(function(e) {
+        console.log(table.rows( { selected: true } ).data());
+        var data = table.rows( { selected: true} ).data();
+        var env = document.createElement('epiviz-environment');
+        if (measurements) {
+            var json = '{';
+            for (var i = 0; i < data.length; i++) {
+                var m = measurements[data[i][4]];   
+
+                json += '"measurement' + i + '":{"id":"' + m["id"] + '"' +  
+                        ',"name":"' + m["name"] +  '"' + 
+                        ',"type":"' + m["type"] + '"' + 
+                        ',"datasourceId":"' + m["datasourceId"] + '"' + 
+                        ',"datasourceGroup":"' + m["datasourcegroup"] + '"' +
+                        ',"dataprovider":"' + 'umd' + '"' + 
+                        ',"formula":' + 'null' +
+                        ',"defaultChartType":' + 'null' + 
+                        ',"annotation":' + 'null' +
+                        ',"minValue":' + m["minValue"] +
+                        ',"maxValue":' + m["maxValue"] + '},';
+            }
+            json.substring(0, json.length-1);
+            json += '}';
+            env.measurements = json;
+            $('#polymercontainer').append(env);
+            //var chart = '<epiviz-' + graph + ' dim-s="' + "['" + "measurement0'," + "'measurement1']" + '"' + '></epiviz-' + graph + '>'
+            var chart = document.createElement('epiviz-' + graph);
+            chart.setAttribute("dim-s", '["measurement0", "measurement1"]');
+            var beforeNode = Polymer.dom($('epiviz-environment')).childNodes[0];
+            Polymer.dom($('epiviz-environment')).insertBefore(chart, beforeNode);
+            // Polymer.dom($('epiviz-environment').root).appendChild(chart);
+            // var chart = '<epiviz-' + graph + ' dim-s="[measurement0","measurement1"]"' + '></epiviz-' + graph + '>'
+            // $('epiviz-environment').append(chart);
+        }
+        $('.ui.modal').hide();
+    });
+    $('.ui.checkbox input[type="checkbox"]').click(function(e) {
+        console.log(this);
+        e.stopPropagation();
+        var split = this.id.split('-');
+        if (split[0] === "source") {
+            var ids = $('.ui.checkbox[id$=' + split[1] + ']');
+            ids.each(function(index) {
+                $(ids[index]).checkbox('set checked');
+                console.log(ids[index]);
+            })
+        }
+    })
+}
+function filter(value, anno, filter) {
+    console.log(measurements);
+    console.log(annotations);
+    console.log(anno);
+    console.log(filters);
+    var all_empty = true;
+    //filtering for this value
+    //if (filter) {
+    console.log(filters[anno].indexOf(value));
+    console.log(value);
+    console.log(filters);
+    if (filter) {
+        filters[anno].push(value);
+    } else {
+        filters[anno].splice(filters[anno].indexOf(value),1);
+    }
+    Object.keys(filters).forEach(function(list) {
+        if (filters[list].length !== 0) {
+            all_empty = false;
+        }
+    });
+    Object.keys(measurements).forEach(function(source) {
+        measurements[source].forEach(function(data) {
+            var hide = false;
+            if (!(all_empty && $('#' + data['id']).css('display') === 'none')) {
+                Object.keys(filters).forEach(function(category) {
+                    if (filters[category].length !== 0) {
+                        if (filters[category].indexOf(data['annotation'][category]) === -1) {
+                            hide = true;
+                        }
+                    }
+                });
+            }
+            if (hide) {
+                $('#' + data['id']).hide();
+            } else {
+                $('#' + data['id']).show();
+            }
+        });
+    });
+}  
---
+++ demo/rendering.js
@@ -0,0 +1,277 @@
+var table;
+var measurements;
+var filters = {};
+var graph;
+var selections = {};
+function rightAccordion() {
+    for (var i = 0; i < 1; i++) {
+        Object.keys(measurements).forEach(function(source) {
+            console.log('this is source' + source);
+            var item = document.createElement('div');
+            var title = document.createElement('a');
+            var titlecheckbox = document.createElement('div');
+            var checkboxlabel = document.createElement('label');
+            var checkboxinput = document.createElement('input');
+            var icon = document.createElement('i');
+            var content = document.createElement('div');
+            var form = document.createElement('div'); 
+            var fields = document.createElement('div');
+            
+            item.className = "item";
+            item.id = source;
+            title.className = "title";
+            titlecheckbox.className = "ui checkbox";
+            titlecheckbox.id = source;
+            checkboxinput.type = "checkbox";
+            checkboxinput.id = "source-" + source;
+            checkboxlabel.innerHTML = source;
+            icon.className = "dropdown icon";
+            content.className = "content";
+            form.className = "ui form";
+            measurements[source].forEach(function(point) {
+                var field = document.createElement('div');
+                var checkbox = document.createElement('div');
+                var input = document.createElement('input');
+                var label = document.createElement('label');   
+                fields.className = "grouped fields";
+                field.className = "field";
+                field.id = point.id;
+                checkbox.className = "ui checkbox";
+                checkbox.id = "item-" + point.id + "-" + source;
+                input.type = "checkbox";
+                //input.id = "item-" + point.id + "-" + source;
+                input.name = "small";
+                label.innerHTML = point.id;     
+
+                fields.appendChild(field);
+                field.appendChild(checkbox);
+                checkbox.appendChild(input);
+                checkbox.appendChild(label);    
+            })
+            item.appendChild(title);
+            item.appendChild(content);
+            title.appendChild(titlecheckbox);
+            title.appendChild(icon);
+            titlecheckbox.appendChild(checkboxinput);
+            titlecheckbox.appendChild(checkboxlabel);
+            content.appendChild(fields);    
+
+            $('#rightmenu').append(item);
+        });
+    }
+    console.log('rightmenu');
+    $('#rightmenu').accordion({
+        exclusive : false,
+        selector : {
+            trigger: '.title .ui.checkbox label'
+        },
+        verbose : true
+    });
+}
+function overlay() {
+    el = document.getElementById("overlay");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+//left menu
+function loadMeasurements() {
+    $.getJSON('./measurements', function(data) {
+        var length = data.result.id.length;
+        var info = data.result;
+        var values;
+        var ranges = {};
+        var checkboxIndex = 0;
+        measurements = {};
+        measurements[data.result.datasourceId] = []
+        annotations = Object.keys(data.result.annotation[0]);
+        console.log(annotations);
+        for (var i = 0; i < length; i++) {
+            var obj = {
+                "id": info.id[i],
+                "name": info.name[i],
+                "type": info.type,
+                "annotation": info.annotation[i],
+                "datasourcegroup": info.datasourceGroup,
+                "defaultChartType": info.defaultChartType,
+                "minValue": info.minValue,
+                "maxValue": info.maxValue,
+                "metadata": info.metadata,
+            }
+            measurements[data.result.datasourceId].push(obj);
+        }
+        console.log(measurements)
+        annotations.forEach(function(text) {
+            var item = document.createElement('div');
+            var title = document.createElement('a');
+            var icon = document.createElement('i');
+            var content = document.createElement('div');
+            var form = document.createElement('div');
+            var fields = document.createElement('div');
+            
+            Object.keys(measurements).forEach(function(data_source) {
+                values = $.unique(measurements[data_source].map(function(id) {
+                    return id.annotation[text];
+                }));
+            });
+            values.sort(function(a, b) {
+                return a - b;
+            });
+            filters[text] = [];
+            if (text === "Age" || text === "index") {
+                var field = document.createElement('div');
+                var range1 = document.createElement('div');
+                var range2 = document.createElement('div');
+                var display1 = document.createElement('span');
+                var display2 = document.createElement('span');
+                var cont1 = document.createElement('p');
+                var cont2 = document.createElement('p');
+
+                field.className = "field";
+                field.width = "inherit";
+                range1.className = "ui range"
+                range1.id = text + "-range1";
+                display1.id = text + "-display1";
+                cont1.innerHTML = "Min: ";
+                cont2.innerHTML = "Max: ";
+                fields.appendChild(field);
+                field.appendChild(range1);
+                cont1.appendChild(display1);
+                field.appendChild(cont1);
+                ranges[range1.id] = values;
+                ranges[range2.id] = values;
+            } else {
+                values.forEach(function(anno) {
+                    var field = document.createElement('div');
+                    var checkbox = document.createElement('div');
+                    var input = document.createElement('input');
+                    var label = document.createElement('label');  
+                    field.className = "field";
+                    checkbox.className = "ui checkbox";
+                    checkbox.id = "checkbox" + checkboxIndex;
+                    input.type = "checkbox"
+                    input.name = anno;
+                    input.value = text + "-" + anno;
+                    label.innerHTML = anno; 
+                    fields.appendChild(field);
+                    field.appendChild(checkbox);
+                    checkbox.appendChild(input);
+                    checkbox.appendChild(label);
+                    checkboxIndex++;
+                });
+            }
+            item.className = "item";
+            item.id = text;
+            title.className = "title";
+            title.innerHTML = text;
+            icon.className = "dropdown icon";
+            content.className = "active content";
+            form.className = "ui form";
+            fields.className = "grouped fields";
+
+            item.appendChild(title);
+            item.appendChild(content);
+            title.appendChild(icon);
+            content.appendChild(fields);
+            $('#leftmenu').append(item);
+        });
+        for (var i = 0; i < checkboxIndex; i++) {
+            $('#checkbox' + i).checkbox({
+                onChecked: function() {
+                    //is checked is always late because it calls onchange first before it changes status
+                    //false means the person just checked the box
+                    /*console.log($(this).);
+                    if ($(this).checkbox("is checked")) {
+                        $(this).checkbox("set unchecked");
+                        unfilter($(this).val());
+                    } else if ($(this).checkbox("is unchecked")) {
+                        $(this).checkbox("set checked");
+                        filter($(this).val());
+                    }*/
+                    filter($(this).val().split("-")[1], $(this).val().split("-")[0], true);
+                },
+                onUnchecked: function() {
+                    filter($(this).val().split("-")[1], $(this).val().split("-")[0], false);
+                }
+            });
+        }
+        $('#leftmenu').accordion({
+            exclusive: false
+        });
+        Object.keys(ranges).forEach(function(ids) {
+            if (ids.charAt(ids.length-1) == '1') {
+                $('#' + ids).range({
+                    min: ranges[ids][0],
+                    max: ranges[ids][ranges[ids].length-1],
+                    start: ranges[ids][0],
+                    values: [ranges[ids][0], ranges[ids][ranges[ids].length-1]],
+                    step: 1,
+                    onChange: function(value) {
+
+                        $('#'+ ids.split('-')[0] + "-display" + ids.charAt(ids.length-1)).html(value);
+                    }
+                });
+            }
+        });
+        $('.active.content').each(function(index) {
+            $('.active.content')[0].className = 'content';
+        })
+        //Right menu
+        rightAccordion();
+        attachActions();
+    });
+}
+function load() {
+    $.getJSON("http://epiviz.cbcb.umd.edu/data/main.php?action=getMeasurements", function(data) {
+        var length = data.data.id.length;
+        var info = data.data;
+        measurements = [];
+        for (var i = 0; i < length; i++) {
+            var obj = {
+                "id": info.id[i],
+                "name": info.name[i],
+                "type": info.type[i],
+                "annotation": info.annotation[i],
+                "datasourcegroup": info.datasourceGroup[i],
+                "datasourceId": info.datasourceId[i],
+                "defaultChartType": info.defaultChartType[i],
+                "minValue": info.minValue[i],
+                "maxValue": info.maxValue[i],
+                "metadata": info.metadata[i],
+            }
+            measurements.push(obj);
+        }
+
+        var mlength = measurements.length;
+
+        $('#tablediv').append('<table id="modalTable" class="display"><thead><tr><th>Id</th><th>name</th><th>type</th><th>datasourcegroup</th><th>Index</th></tr></thead></table>');
+
+        tableString= '<tbody>';
+
+        for (var i=0; i< mlength; i++) {
+            tableString += '<tr><td>' 
+            + measurements[i].id + '</td><td>' + measurements[i].name + '</td><td>' + measurements[i].type + '</td><td>' + measurements[i].datasourcegroup+ '</td><td>' + i + '</td></tr>';
+        }
+
+        tableString += '</tbody>';
+        $('#modalTable').append(tableString);
+        table = $('#modalTable').DataTable({
+              aoColumns : [
+                { sWidth: '25%' },
+                { sWidth: '25%' },
+                { sWidth: '20%' },
+                { sWidth: '25%' },
+                { sWidth: '5%' },
+              ],
+            select: {style: 'os'},
+            buttons: [
+                {
+                    text: 'Finish',
+                    action: function ( e, dt, node, config ) {
+                        console.log('clicked');
+                    }
+                }
+            ]
+        });
+        console.log(table.rows('.selected').data());
+        console.log(table.rows( { selected: true } ).data());
+    });
+}
